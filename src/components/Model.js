42426e23de58bb45c6d671e8c68dd83454628c7d
sawantatharva <atharva.sawant@plaksha.edu.in>
2023-04-15 18:56:18 +0530

    Completed - Landing Page & Linking Pages

--- src/components/Model.js
+++ src/components/Model.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { FiChevronDown } from 'react-icons/fi'
 
 const Model = () => {
-  return (
-    <div>Model</div>
-  )
+    return (
+        <div>
+            <div className='absolute inset-x-8 top-[15%] text-center'>
+                <h1 className='text-4xl font-bold'>Model Y</h1>
+                <p className='pt-1 font-light'><a href='https://www.tesla.com/inventory/new/my'><span className='text-sm underline underline-offset-4'>View Inventory</span></a></p>
+            </div>
+            <div className='flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]'>
+                <button className='bg-[#393c41] rounded-md w-96 lg:w-64 lg:mx-4 h-10 mt-2 text-white'><a href='https://www.tesla.com/modely/design#overview'>Order Now</a></button>
+                <button className='bg-[#f4f4f4] rounded-md w-96 lg:w-64 lg:mx-4 h-10 mt-2'><a href='https://www.tesla.com/drive'>Demo Drive</a></button>
+            </div>
+            <div className='flex justify-center animate-bounce absolute inset-x-0 bottom-[3%]'>
+                <FiChevronDown size={24}/>
+            </div>
+        </div>
+    )
 }
 
-export default Model
+export default Model;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,25 +5,27 @@ const Navbar = () => {
     return (
         <div className='flex justify-between items-center px-12 p-4 text-sm font-bold'>
             <div>
-                <img className="h-3" src={TeslaLogo} alt="Tesla Logo" />
+                <a href='https://www.tesla.com/'>
+                    <img className="h-3" src={TeslaLogo} alt="Tesla Logo" />
+                </a>
             </div>
 
             <div className='pl-20 hidden lg:inline'>
                 <ul className='flex justify-center hover:cursor-pointer'>
-                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'>Model S</li>
-                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'>Model 3</li>
-                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'>Model X</li>
-                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'>Model Y</li>
-                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'>Solar Roof</li>
-                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'>Solar Panels</li>
-                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'>Powerwall</li>
+                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'><a href='https://www.tesla.com/models'>Model S</a></li>
+                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'><a href='https://www.tesla.com/model3'>Model 3</a></li>
+                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'><a href='https://www.tesla.com/modelx'>Model X</a></li>
+                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'><a href='https://www.tesla.com/modely'>Model Y</a></li>
+                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'><a href='https://www.tesla.com/solarroof'>Solar Roof</a></li>
+                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'><a href='https://www.tesla.com/solarpanels'>Solar Panels</a></li>
+                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'><a href='https://www.tesla.com/powerwall'>Powerwall</a></li>
                 </ul>
             </div>
 
             <div className=' hidden lg:inline'>
                 <ul className='flex justify-center hover:cursor-pointer'>
-                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'>Shop</li>
-                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'> Account</li>
+                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'><a href='https://shop.tesla.com/'>Shop</a></li>
+                    <li className='py-1 px-3 hover:rounded hover:bg-black/5'><a href='https://www.tesla.com/teslaaccount'>Account</a></li>
                     <li className='py-1 px-3 hover:rounded hover:bg-black/5'>Menu</li>
                 </ul>
             </div>
